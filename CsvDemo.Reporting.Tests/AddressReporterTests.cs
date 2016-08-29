afc7c89ec3d842a1eb7899474d69504a35fd8292
Ron MacNeil <macro@hotmail.com>
2016-08-29 22:09:24 +1000

    Add tests for report generators

---
+++ CsvDemo.Reporting.Tests/AddressReporterTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CsvDemo.Reporting.Tests
+{
+    [TestClass]
+    public class AddressReporterTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GenerateReport_Null_Addresses_Throws_ArgumentNullException()
+        {
+            AddressReporter.GenerateReport(null, new StringWriter());
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GenerateReport_Null_Destination_Throws_ArgumentNullException()
+        {
+            AddressReporter.GenerateReport(new string[0], null);
+        }
+
+
+        [TestMethod]
+        public void GenerateReport_Generates_Correct_Report()
+        {
+            var addresses =
+                new[] {
+                    "1 Cantaloupe Cres",
+                    "3 Banana Blvd",
+                    "2 Banana Blvd",
+                    "4 Apple Ave" };
+            
+            var correctReport =
+                string.Join(
+                    Environment.NewLine,
+                    "Address",
+                    "4 Apple Ave",
+                    "2 Banana Blvd",
+                    "3 Banana Blvd",
+                    "1 Cantaloupe Cres");
+
+            var stringWriter = new StringWriter();
+
+            AddressReporter.GenerateReport(addresses, stringWriter);
+
+            Assert.AreEqual(correctReport.Trim(), stringWriter.ToString().Trim());
+        }
+
+    }
+}
---
+++ CsvDemo.Reporting.Tests/NameFrequencyReporterTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsvDemo.Reporting.Tests
+{
+    [TestClass]
+    public class NameFrequencyReporterTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GenerateReport_Null_NameTallies_Throws_ArgumentNullException()
+        {
+            NameFrequencyReporter.GenerateReport(null, new StringWriter());
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GenerateReport_Null_Destination_Throws_ArgumentNullException()
+        {
+            NameFrequencyReporter.GenerateReport(new Dictionary<string,int>(), null);
+        }
+
+
+        [TestMethod]
+        public void GenerateReport_Generates_Correct_Report()
+        {
+            var tally =
+                new Dictionary<string, int>() {
+                    { "a", 1 },
+                    { "c", 3 },
+                    { "b", 3 },
+                    { "d", 4 }};
+
+            var correctReport =
+                string.Join(
+                    Environment.NewLine,
+                    "Name,Frequency",
+                    "d,4",
+                    "b,3",
+                    "c,3",
+                    "a,1");
+
+            var stringWriter = new StringWriter();
+
+            NameFrequencyReporter.GenerateReport(tally, stringWriter);
+
+            Assert.AreEqual(correctReport.Trim(), stringWriter.ToString().Trim());
+        }
+
+    }
+}
