0bbba54ec2aed83824630ef824e05f627a2cf02d
Ron MacNeil <macro@hotmail.com>
2016-08-29 20:31:46 +1000

    CsvDemo.Console: Remove unnecessary usings

--- CsvDemo.Console/Program.cs
+++ CsvDemo.Console/Program.cs
@@ -1,9 +1,7 @@
 ﻿using CsvHelper;
 using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using CsvDemo.Analysers;
 using CsvDemo.Reporting;
